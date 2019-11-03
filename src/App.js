b49611ef782cbfde4fe151d581183404a3eb8e9f
Glitch (starter-react-template) <none>
2019-11-03 00:57:50 +0000

    🚌🛣 Checkpoint ./src/App.js:1036401/123

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import theme from '@rebass/preset';
-import { Heading, Box } from 'rebass';
+import { Heading, Box, Flex, Text } from 'rebass';
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
         <Box variant='styles.root'>
+          <Flex>
+            <Text>Wantodo</Text>
+            <Text>Profile</Text>
+          </Flex>
           <Heading>
             Hello, World
           </Heading>
