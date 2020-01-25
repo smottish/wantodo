50c7570bba71651a0657af9e3c9b37ce465fcd01
Glitch (starter-react-template) <none>
2020-01-25 20:40:41 +0000

    ⚓️🚦 Checkpoint ./src/MediaQuery.js:1036401/859

--- src/MediaQuery.js
+++ src/MediaQuery.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 class MediaQuery extends Component {
   constructor() {
-    this.mql = null;
+    this.state = {
+      matches: false,
+    }
+    this.mql = null
     this.onMediaQuery = this.onMediaQuery.bind(this)
   }
   
   componentDidMount() {
-    this.mql = window.matchMedia(this.props.query)
+    const mql = window.matchMedia(this.props.query)
+    const matches = mql.matches
+    
+    mql.addListener(this.onMediaQuery)
+    this.setState({ matches })
+    if (this.props.onChange) {
+      this.props.onChange(matches)
+    }
+    
+    this.mql = mql
   }
   
-  onMediaQuery(match) {
-    
+  onMediaQuery(mq) {
+    this.setState({ matches: mq.matches })
+    if ((mq.matches !== this.state.matches) && this.props.onChange) {
+      this.props.onChange(mq.matches)
+    }
   }
 }
 
+MediaQuery.propTypes = {
+  query: PropTypes.string,
+  onChange: PropTypes.func,
+}
