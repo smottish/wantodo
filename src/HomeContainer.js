b7002ccef75669ae84425b2fb79d2d34f5c3e62a
Glitch (starter-react-template) <none>
2020-02-10 00:12:05 +0000

    🐋🌏 Checkpoint ./src/HomeContainer.js:1036401/539 ./package.json:1036401/1233

--- src/HomeContainer.js
+++ src/HomeContainer.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Heading } from 'rebass';
+import { Heading, Flex } from 'rebass';
+import { Input } from '@rebass/forms';
 
 class HomeContainer extends Component {
+  constructor() {
+    this.onChangeWant = this.onChangeWant.bind(this)
+  }
+  
+  onChangeWant(arg1, ...args) {
+    console.log(arg1)
+    console.log(args)
+  }
+
   render() {
     return (
       <>
-        <Heading>What should I do?</Heading>
+        <Flex justifyContent='center'><Heading>I want to...</Heading></Flex>
+        <Input defaultValue='Enter something you want to do!' onChange={this.onChangeWant}/>
       </>
     )
   }
