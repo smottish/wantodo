c1b4b2b47664e6956b6c2209de0b43c63b244cb8
Glitch (starter-react-template) <none>
2020-02-10 00:24:02 +0000

    🎥🎀 Checkpoint ./src/HomeContainer.js:1036401/294

--- src/HomeContainer.js
+++ src/HomeContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Heading, Flex } from 'rebass';
+import { Heading, Flex, Box, Button } from 'rebass';
 import { Input } from '@rebass/forms';
+import { withTheme } from 'emotion-theming';
 
 class HomeContainer extends Component {
   constructor() {
+    super()
     this.onChangeWant = this.onChangeWant.bind(this)
   }
   
@@ -16,10 +18,15 @@ class HomeContainer extends Component {
     return (
       <>
         <Flex justifyContent='center'><Heading>I want to...</Heading></Flex>
-        <Input defaultValue='Enter something you want to do!' onChange={this.onChangeWant}/>
+        <Flex justifyContent='center'>
+          <Box width={[1, 1, 2/3]}>
+            <Input defaultValue='Enter something you want to do!' onChange={this.onChangeWant}/>
+            <Button>Add</Button>
+          </Box>
+        </Flex>
       </>
     )
   }
 }
 
-export default HomeContainer;
+export default withTheme(HomeContainer);
