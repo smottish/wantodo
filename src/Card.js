49a7a43bd3d4cd9317d331ed77c8119ec6db6635
Glitch (wantodo-proto) <none>
2020-06-28 21:48:19 +0000

    💳🎰 Checkpoint ./src/Card.js:1036401/636

---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { useTheme } from 'emotion-theming';
+
+const Card = styled.div`
+  box-shadow: 0 2px 2px 0px rgba(0, 0, 0, 0.2);
+  padding: 5px;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid rgba(0, 0, 0, 0.07);
+`
+
+const CardPrimary = styled.div`
+  padding: 10px;
+`
+
+const CardActions = styled.div`
+
+`
