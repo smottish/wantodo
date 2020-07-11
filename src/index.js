d97f78ee9e65a325c1434b7bbd27837ccea90a4b
Glitch (wantodo-proto) <none>
2020-07-11 18:12:21 +0000

    ☃️⛪️ Checkpoint ./src/WantsProvider.js:1036401/1702 ./src/index.js:1036401/51

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import theme from './theme.js'
 import { ToastProvider } from './ToastProvider.js'
 import { ThemeProvider } from 'emotion-theming';
+import { WantsProvider } from './WantsProvider.js'
 import './index.css'
 
 ReactDOM.render(
