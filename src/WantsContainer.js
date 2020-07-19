ed035f6ca8ae65c9ca508dd5e66e351bcb4ee60a
Glitch (wantodo-proto) <none>
2020-07-19 00:21:29 +0000

    💨🍛 Checkpoint ./src/WantsContainer.js:1036401/964

--- src/WantsContainer.js
+++ src/WantsContainer.js
@@ -16,12 +16,20 @@ const WantCardReadOnly = ({ want, onEdit, onDelete }) => (
 )
 
 const WantCardEditable = ({ want, onSave, onCancel }) => {
+  // TODO SM (2020-07-18): Do some more research on best practices for implementing
+  // editable forms that get populated from API data. Another approach would be to
+  // implement this as a controlled form, which just means there'd be no component
+  // state and the parent would be responsible for handling changes to props that
+  // get passed here. The following resources are where I got the inspiration for
+  // using useEffect + useState:
+  // https://www.taniarascia.com/crud-app-in-react-with-hooks/
+  // https://learnwithparam.com/blog/how-to-pass-props-to-state-properly-in-react-hooks/
   const [ editableWant, setEditableWant ] = useState(want)
   useEffect(() => {
-    // If want prop changes, update editable title
+    // If want prop changes, update editable want
     setEditableWant(want)
   }, [want] /* Only fire when title changes */)
-  const changeDesc = (ev) => 
+  const changeDesc = (ev) =>
     setEditableWant({ ...editableWant, description: ev.target.value})
   
   return <Card css="margin:10px">
@@ -39,8 +47,8 @@ const WantCardEditable = ({ want, onSave, onCancel }) => {
 }
 
 const WantCardContainer = ({ editable, want, ...props }) => {
-  if (want.id === editable) {
-    return <WantCardEditable want={want} {...props}/>
+  if (editable && want.id === editable.id) {
+    return <WantCardEditable want={editable} {...props}/>
   } else {
     return <WantCardReadOnly want={want} {...props}/>
   }
@@ -58,6 +66,10 @@ function WantsContainer(props) {
   function onCreateSuccess(want) {
     // TODO SM (2020-07-11): For now, just add the want to the end of the list
     setWants([ ...wants, want ])
+  }
+  
+  function onChange(name, value) {
+    setEditable({ ...editable, })
   }
   
   function onSave(updatedWant) {
@@ -86,10 +98,11 @@ function WantsContainer(props) {
         key={want.id}
         want={want}
         editable={editable}
-        onEdit={() => setEditable(want.id)}
-        onSave={onSave} onCancel={onCancel}
+        onEdit={() => setEditable({ ...want })}
+        onSave={onSave}
+        onCancel={onCancel}
       />
-    )} 
+    ))} 
   </>
 }
 
