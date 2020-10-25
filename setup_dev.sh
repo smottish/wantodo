69acff3cf2b4e4de54399d129d25275c22e2a529
Shane Mottishaw <shane@automottic.ca>
2020-10-24 19:58:35 -0700

    Add script to setup bash development environment

---
+++ setup_dev.sh
@@ -0,0 +1,5 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo "Run 'nvm use node' to switch to latest node version."
+echo "When done, run 'nvm use system' to switch to the system installed node"
