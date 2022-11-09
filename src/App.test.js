1d04e3be3de1efbe1d560353c8f80bd559a1ccf1
Shane Mottishaw <shane@automottic.ca>
2022-11-08 16:35:12 -0800

    1) Update README 2) Fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import theme from './theme.js'
+import { ThemeProvider } from 'emotion-theming';
+import { ToastProvider } from './ToastProvider.js'
+
+const mockLocalStorage = (function () {
+  const store = { token: "abcd" };
+  return {
+    getItem: function(key) {
+      return store[key] || null;
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+  }
+})();
+
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: false,
+    value: () => ({
+      matches: true,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }),
+  })
+
+  Object.defineProperty(window, 'localStorage', {
+    value: mockLocalStorage,
+  });
+
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </ThemeProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
